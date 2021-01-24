f94b15a1177e2c440dee6e3740990053439bdebe
Skrypnykov <oskripnikov@gmail.com>
2021-01-24 12:46:28 +0200

    add lesson 6

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <ul>
+        <li>CSS</li>
+        <li>HTML</li>
+        <li>Javascript</li>
+        <li>React</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
