6101170ac011937b555d3e9a3e394f5cbe85b87f
Oleh Skrypnykov <oskripnikov@gmail.com>
2021-02-09 23:04:29 +0200

    lesson css-modules

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
         <nav className="nav">
-            <ul>
-                <li><a href="#q">Profile</a></li>
-                <li><a href="#q">Messages</a></li>
-                <li><a href="#q">News</a></li>
-                <li><a href="#q">Music</a></li>
-                <li><a href="#q">Setting</a></li>
+            <ul className="nav-list">
+                <li className="nav-item"><a href="#q">Profile</a></li>
+                <li className="nav-item"><a href="#q">Messages</a></li>
+                <li className="nav-item"><a href="#q">News</a></li>
+                <li className="nav-item"><a href="#q">Music</a></li>
+                <li className="nav-item"><a href="#q">Setting</a></li>
             </ul>
         </nav>
     );
--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
+import './Profile.css';
 
 const Profile = () => {
     return (
         <div className="content">
-            <div className="content-image-header">
+            <div className="header-wrap">
                 <img src="https://miro.medium.com/max/3200/0*cGDKbUrA_8vJC4d3" alt=""/>
             </div>
-            <div className="content-description-wrapper">
-                <img src="https://cdn1.iconfinder.com/data/icons/robotics/128/_Artificial_Intelligiance-512.png" alt=""/>
-                <div className="content-description">
-                    <h4 className="content-description-title">Oleg S.</h4>
-                    <p className="content-description-date">Date of Birth: 24 August</p>
-                    <p className="content-description-city">City: Dobropillya</p>
-                    <p className="content-description-education">Education: School '19</p>
-                    <p className="content-description-web">Web:	&nbsp; <a href="https://skrypnykov.github.io/" target="_blank
+            <div className="description-wrap">
+                <img className='description-avatar' src="https://cdn1.iconfinder.com/data/icons/robotics/128/_Artificial_Intelligiance-512.png" alt=""/>
+                <div className="description-content">
+                    <h4 className="description-title">Oleg S.</h4>
+                    <p className="description-date">Date of Birth: &nbsp;24 August</p>
+                    <p className="description-city">City: &nbsp;Dobropillya</p>
+                    <p className="description-education">Education: &nbsp;School '19</p>
+                    <p className="description-web">web:	&nbsp;<a href="https://skrypnykov.github.io/" target="_blank
 ">https://skrypnykov.github.io/</a></p>
                 </div>
             </div>
             <div className="content-posts">
-                <h3 className="content-posts-title">My posts</h3>
-                <div className="content-post-new">
+                <h3 className="posts-title">My posts</h3>
+                <div className="post-new">
                     <textarea name="comment" cols="100" rows="3" placeholder="your news..."></textarea><br/>
                     <input type="submit" value="Send"/>
                 </div>
-                <div>
-                    <div>
-                        post 1
+                <div className='post-message'>
+                    <div className='post-item'>
+                        <img src="https://www.pngkey.com/png/full/353-3534482_avatar-portfolio-02-avatar-sketch-cartoon-avatar.png" alt=""/>
+                        <p>Hey, why nobody love me?</p>
                     </div>
-                    <div>
-                        post 2
-                    </div>
-                    <div>
-                        post 3
+                    <div className='post-item'>
+                        <img src="https://www.pngkey.com/png/full/87-879351_grapefruit-balm-baby-rawlemon-raw-pure-essential-oil.png" alt=""/>
+                        <p>It's our new program! Hey!</p>
                     </div>
                 </div>
             </div>
