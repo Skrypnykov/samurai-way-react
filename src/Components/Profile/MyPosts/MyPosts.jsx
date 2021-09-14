063ac9a2e9b32e3f05e893e210d7809d29dcc6c9
Skrypnykov <oskripnikov@gmail.com>
2021-09-14 11:44:04 +0300

    update profile

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -4,14 +4,9 @@ import Post from "./Post/Post";
 
 const MyPosts = (props) => {
   let postsEletemts = props.postsData.map((p) => (
-    <Post
-      key={p.id}
-      avatar={p.avatar}
-      message={p.message}
-      like={p.like}
-      dislike={p.dislike}
-    />
-  ));
+    <Post key={p.id} avatar={p.avatar} message={p.message} like={p.like} dislike={p.dislike} />
+    )
+  );
 
   let newPostElement = React.createRef();
 
@@ -28,16 +23,8 @@ const MyPosts = (props) => {
     <div className={s.posts}>
       <h3 className={s.postsTitle}>My posts</h3>
       <div className={s.postNew}>
-        <textarea
-          onChange={onPostChange}
-          ref={newPostElement}
-          name="comment"
-          cols="100"
-          rows="3"
-          placeholder="your post..."
-          value={props.newPostText}
-        />
-        <br />
+        <textarea onChange={onPostChange} ref={newPostElement} name="comment" cols="100" rows="3" placeholder="your post..." value={props.newPostText} />
+        <br/>
         <input type="submit" value="Add post" onClick={addPost} />
       </div>
       {postsEletemts}
