05629b8195a3ce0ac58e3e6adc7c583be98a607b
Skrypnykov <oskripnikov@gmail.com>
2021-12-06 22:58:44 +0200

    update ProfileContainer userId

--- src/Components/Profile/ProfileContainer.jsx
+++ src/Components/Profile/ProfileContainer.jsx
@@ -10,7 +10,7 @@ class ProfileContainer extends React.Component {
   componentDidMount () {
     let userId = this.props.match.params.userId;
     if(!userId) {
-      userId = 2
+      userId = 20951
     };
     axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId)
       .then((response) => {
