c3b01ed636e8b27f15e9e161b890b3788ec03936
Skrypnykov <oskripnikov@gmail.com>
2021-12-24 15:07:03 +0200

    install redux-form

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import s from "./Login.module.css";
 
+const LoginForm =(props) => {
+    return (
+        <form>
+            <div>
+                <input placeholder={"Login"} />
+            </div>
+            <div>
+                <input placeholder={"Password"} />
+            </div>
+            <div>
+                <input type={"checkbox"} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
 const Login =(props) => {
-    return <div className={s.title}>LOGIN</div>
+    return <div>
+        <div className={s.title}>LOGIN</div>
+        <LoginForm />
+    </div>
 }
 
 export default Login;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,13 +5,15 @@ import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from "redux-form";
 
 let reducers = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
     sidebar: sidebarReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
